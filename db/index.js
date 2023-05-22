952afd4e5ffce008f0a99a9c81e1fc7f7ecb1991
mBeanland1 <mbeanland@scottlogic.com>
2023-05-22 10:11:31 +0100

    initial commit
    - added create-react-app react stuff
    - added starter db script

---
+++ db/index.js
@@ -0,0 +1,18 @@
+const EloRank = require('elo-rank');
+
+
+//TODO modify to include update code
+// exports.handler = async (event) => {
+//   // TODO implement
+//   const movie = {
+//     title: faker.lorem.words(),
+//     plot: faker.lorem.paragraph(),
+//     director: `${faker.name.firstName()} ${faker.name.lastName()}`,
+//     image: faker.image.abstract(),
+//   };
+//   const response = {
+//     statusCode: 200,
+//     body: JSON.stringify(movie),
+//   };
+//   return response;
+// };
