8ef22c2ba975a5ee6219a5c17333c02b7bf18ed3
mBeanland1 <mbeanland@scottlogic.com>
2023-05-22 16:34:43 +0100

    added basic script for updating elo scores in db

--- db/updateElo/index.js
+++ db/updateElo/index.js
@@ -1,5 +1,6 @@
-const EloRank = require('elo-rank');
 const { MongoClient } = require("mongodb");
+const EloRank = require('elo-rank');
+const elo = new EloRank();
  
 // Replace the following with your Atlas connection string                                                                                                                                        
 const url = "secret";
@@ -7,32 +8,120 @@ const client = new MongoClient(url);
  
  // The database to use
  const dbName = "felo";
-                      
- async function run() {
+
+async function updateUserInDB(username, elo) {
+    console.log("\nusername: "  + username)
+    console.log("new elo: " + elo)
     try {
-         await client.connect();
-         console.log("Connected correctly to server");
-         const db = client.db(dbName);
-         const col = db.collection("users");
-         let userDocument = {
-             "username": "insertTest",
-             "elo": 1000
-         }
-         await col.updateOne({ username: "insertTest"},
-        {
-            $set: {
-                elo: 3000
-            },
-        }
+        await client.connect();
+        const db = client.db(dbName);
+        const col = db.collection("users");
+        await col.updateOne({ username: username},
+            {
+                $set: {
+                    elo: elo
+                },
+            }
         )
 
-        } catch (err) {
-         console.log(err.stack);
-     }
+    } catch (err) {
+        console.log(err.stack);
+    }
  
-     finally {
+    finally {
         await client.close();
     }
 }
-run().catch(console.dir);
 
+async function saveGame(game) {
+    try {
+        await client.connect();
+        const db = client.db(dbName);
+        const col = db.collection("games");
+        game.creationDate = new Date();
+        await col.insertOne(game);
+
+       } catch (err) {
+        console.log(err.stack);
+    }
+
+    finally {
+       await client.close();
+   }
+}
+
+
+async function updateElo(teams) {
+    for (team of teams) {
+        team.meanElo = (team.players[0].elo + team.players[1].elo) / 2;
+    }
+
+
+    teams[0].expectedScore = elo.getExpected(teams[0].meanElo, teams[1].meanElo);
+    teams[1].expectedScore = elo.getExpected(teams[1].meanElo, teams[0].meanElo);
+
+    for (team of teams) {
+        for (user of team.players) {
+            await updateUserInDB(user.username, elo.updateRating(team.expectedScore, team.verdict, user.elo));
+        }
+    }
+}
+
+
+async function retrieveUserFromDB(username) {
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const users = database.collection("users");
+        const query = { username: username };
+        return await users.findOne(query);
+      } finally {
+        await client.close();
+      }
+}
+
+async function retrievePlayersFromDB(input) {
+    players = [];
+    for (username of input.players) {
+        players.push(await retrieveUserFromDB(username));
+    }
+    return players;
+}
+
+async function mockLambdaCall(event) {
+
+    teams = [];
+
+    // retrieve user info based on usernames
+    for (team of event.teams) {
+        teams.push(
+            {
+                players: await retrievePlayersFromDB(team),
+                verdict: team.score === 10 ? 1 : 0
+            }
+        )
+    }
+
+    // calculate new elo scores & save for each user
+    await updateElo(teams);
+
+    // save game to history
+    await saveGame(event);
+
+}
+
+
+input = {
+    "teams": [
+        {
+            "players": ["test1", "test2"],
+            "score": 10
+        },
+        {
+            "players": ["test3", "insertTest"],
+            "score": 7
+        }
+    ]
+}
+
+mockLambdaCall(input);
