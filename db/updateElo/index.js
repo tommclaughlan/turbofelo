3cc0c1f076d022b51213369ec3435636b15101b9
unknown <andrew_nomie@msn.com>
2023-06-23 11:11:37 +0100

    Update data structure

--- db/updateElo/index.js
+++ db/updateElo/index.js
@@ -1,11 +1,11 @@
-const EloRank = require('elo-rank');
+const EloRank = require("elo-rank");
 const elo = new EloRank();
 // Import the MongoDB driver
 const MongoClient = require("mongodb").MongoClient;
- 
-// Replace the following with your Atlas connection string                                                                                                                                        
+
+// Replace the following with your Atlas connection string
 const MONGODB_URI = "secret";
- 
+
 // The database to use
 const dbName = "felo";
 
@@ -53,13 +53,14 @@ async function closeConnection() {
 async function updateUserInDB(username, elo) {
     const db = await connectToDatabase();
     const col = db.collection("users");
-    await col.updateOne({ username: username},
+    await col.updateOne(
+        { username: username },
         {
             $set: {
-                elo: elo
+                elo: elo,
             },
         }
-    )
+    );
 }
 
 async function saveGame(game) {
@@ -75,112 +76,98 @@ async function updateElos(newElos) {
     }
 }
 
-
 async function retrievePlayersFromDB(usernames) {
     const db = await connectToDatabase();
     const users = db.collection("users");
-    const query = { username: {$in: usernames} };
+    const query = { username: { $in: usernames } };
     return await users.find(query).toArray();
 }
 
 async function retrievePlayerDictionaryFromDB(usernames) {
     const dbPlayers = await retrievePlayersFromDB(usernames);
 
-    return dbPlayers.reduce((acc, player) => ({
-        ...acc,
-        [player.username]: player
-    }), {});
+    return dbPlayers.reduce(
+        (acc, player) => ({
+            ...acc,
+            [player.username]: player,
+        }),
+        {}
+    );
 }
 
 const calculateElos = (teams) => {
-    const teamElos = teams.map(team => (
-        team.players.reduce((sum, player) => sum + player.elo, 0) / team.players.length
-    ));
+    const teamElos = teams.map(
+        (team) =>
+            team.players.reduce((sum, player) => sum + player.elo, 0) /
+            team.players.length
+    );
 
     const expectedScores = [
         elo.getExpected(teamElos[0], teamElos[1]),
-        elo.getExpected(teamElos[1], teamElos[0])
+        elo.getExpected(teamElos[1], teamElos[0]),
     ];
 
     const newElos = {};
 
     teams.forEach((team, teamIndex) => {
-        team.players.forEach(player => {
-            newElos[player.username] = elo.updateRating(expectedScores[teamIndex], team.verdict, player.elo);
-        })
+        team.players.forEach((player) => {
+            newElos[player.username] = elo.updateRating(
+                expectedScores[teamIndex],
+                team.verdict,
+                player.elo
+            );
+        });
     });
 
     return newElos;
-}
+};
 
 exports.handler = async (event, context) => {
-
     /* By default, the callback waits until the runtime event loop is empty before freezing the process and returning the results to the caller. Setting this property to false requests that AWS Lambda freeze the process soon after the callback is invoked, even if there are events in the event loop. AWS Lambda will freeze the process, any state data, and the events in the event loop. Any remaining events in the event loop are processed when the Lambda function is next invoked, if AWS Lambda chooses to use the frozen process. */
     context.callbackWaitsForEmptyEventLoop = false;
-    
+
     const game = JSON.parse(event.body);
-    
-    const usernames = game.teams.flatMap(team => team.players);
+
+    const usernames = game.teams.flatMap((team) => team.players);
     const dbPlayers = await retrievePlayerDictionaryFromDB(usernames);
 
-    const results = game.teams.map(team => ({
-        players: team.players.map(username => dbPlayers[username]),
-        verdict: Number(team.score) === 10 ? 1 : 0
+    const results = game.teams.map((team) => ({
+        players: team.players.map((username) => dbPlayers[username]),
+        verdict: Number(team.score) === 10 ? 1 : 0,
     }));
 
     const newElos = calculateElos(results);
 
     await updateElos(newElos);
-    
-        const gameToSave = {
+
+    const gameToSave = {
         teams: [
-            {
-                players: [
-                    {
-                        username: usernames[0],
-                        elo: newElos[usernames[0]]
-                    },
-                    {
-                        username: usernames[1],
-                        elo: newElos[usernames[1]]
-                    }
-                ],
-                score: game.teams[0].score
-            },
-            {
-                players: [
-                    {
-                        username: usernames[2],
-                        elo: newElos[usernames[2]]
-                    },
-                    {
-                        username: usernames[3],
-                        elo: newElos[usernames[3]]
-                    }
-                ],
-                score: game.teams[1].score
-            }
-        ]
-    }
+            [usernames[0], usernames[1]],
+            [usernames[2], usernames[3]],
+        ],
+        score: [game.teams[0].score, game.teams[1].score],
+    };
 
     await saveGame(gameToSave);
 
     const db = await connectToDatabase();
-    const users = await db.collection("users")
-    .find({})
-    .sort({elo: -1})
-    .toArray();
-    
-    const games = await db.collection("games")
-    .find({})
-    .sort({creationDate: -1})
-    .limit(6)
-    .toArray();
+    const users = await db
+        .collection("users")
+        .find({})
+        .sort({ elo: -1 })
+        .toArray();
+
+    const games = await db
+        .collection("games")
+        .find({})
+        .sort({ creationDate: -1 })
+        .limit(6)
+        .toArray();
 
     const responseBody = {
-        "users": users,
-        "game": games
-    }
+        users: users,
+        game: games,
+    };
 
     const response = {
         statusCode: 200,
@@ -190,5 +177,4 @@ exports.handler = async (event, context) => {
     await closeConnection();
 
     return response;
-}
-
+};
--- ui/src/pages/home/Home.jsx
+++ ui/src/pages/home/Home.jsx
@@ -10,120 +10,122 @@ import LatestGames from "../../components/latestGames/LatestGames";
 import Page from "../../layouts/Page";
 
 function Home() {
-  const [showRegister, setShowRegister] = useState(false);
-  const [showSubmit, setShowSubmitScore] = useState(false);
-  const [showSubmitMulti, setShowSubmitMultiScore] = useState(false);
-  const [userArray, setUserArray] = useState(null);
-  const [gamesArray, setGamesArray] = useState(null);
+    const [showRegister, setShowRegister] = useState(false);
+    const [showSubmit, setShowSubmitScore] = useState(false);
+    const [showSubmitMulti, setShowSubmitMultiScore] = useState(false);
+    const [userArray, setUserArray] = useState(null);
+    const [gamesArray, setGamesArray] = useState(null);
 
-  useEffect(() => {
-    fetch(
-      ` https://7wpo57scz7.execute-api.eu-north-1.amazonaws.com/default/getUsers`
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setUserArray(result);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-  }, []);
+    useEffect(() => {
+        fetch(
+            ` https://7wpo57scz7.execute-api.eu-north-1.amazonaws.com/default/getUsers`
+        )
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setUserArray(result);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+    }, []);
 
-  useEffect(() => {
-    fetch(
-      ` https://mn2x2tur8c.execute-api.eu-north-1.amazonaws.com/default/retrieveGames`
-    )
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          setGamesArray(result);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-  }, []);
+    useEffect(() => {
+        fetch(
+            ` https://mn2x2tur8c.execute-api.eu-north-1.amazonaws.com/default/retrieveGames`
+        )
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setGamesArray(result);
+                },
+                (error) => {
+                    console.log(error);
+                }
+            );
+    }, []);
 
-  return (
-    <>
-      <Modal
-        show={showRegister}
-        handleClose={() => setShowRegister(false)}
-        children={
-          <RegisterUser
-            setUserArray={setUserArray}
-            setShowRegister={setShowRegister}
-            userArray={userArray}
-          />
-        }
-      />
-      <Modal
-        show={showSubmit}
-        handleClose={() => setShowSubmitScore(false)}
-        children={
-          <SubmitScore
-            setUserArray={setUserArray}
-            setGamesArray={setGamesArray}
-            setShowSubmitScore={setShowSubmitScore}
-            userArray={userArray}
-          />
-        }
-      />
-      <Modal
-        show={showSubmitMulti}
-        handleClose={() => setShowSubmitMultiScore(false)}
-        children={
-          <SubmitMultiScore
-            setUserArray={setUserArray}
-            setGamesArray={setGamesArray}
-            setShowSubmitMultiScore={setShowSubmitMultiScore}
-            userArray={userArray}
-          />
-        }
-      />
-      <Page>
-        <div className="section action-buttons">
-          <div className="columns is-mobile">
-            <div className="column is-one-fifth">
-              <div>
-                <div className="is-flex-direction-column">
-                  <button
-                    className="button is-warning is-medium action-button"
-                    type="button"
-                    onClick={() => setShowSubmitScore(true)}
-                  >
-                    Submit Score
-                  </button>
-                  <button
-                    className="button is-danger is-medium action-button"
-                    type="button"
-                    onClick={() => setShowSubmitMultiScore(true)}
-                  >
-                    Submit 3 Scores
-                  </button>
-                  <button
-                    className="button is-info is-medium action-button"
-                    type="button"
-                    onClick={() => setShowRegister(true)}
-                  >
-                    Register User
-                  </button>
+    return (
+        <>
+            <Modal
+                show={showRegister}
+                handleClose={() => setShowRegister(false)}
+                children={
+                    <RegisterUser
+                        setUserArray={setUserArray}
+                        setShowRegister={setShowRegister}
+                        userArray={userArray}
+                    />
+                }
+            />
+            <Modal
+                show={showSubmit}
+                handleClose={() => setShowSubmitScore(false)}
+                children={
+                    <SubmitScore
+                        setUserArray={setUserArray}
+                        setGamesArray={setGamesArray}
+                        setShowSubmitScore={setShowSubmitScore}
+                        userArray={userArray}
+                    />
+                }
+            />
+            <Modal
+                show={showSubmitMulti}
+                handleClose={() => setShowSubmitMultiScore(false)}
+                children={
+                    <SubmitMultiScore
+                        setUserArray={setUserArray}
+                        setGamesArray={setGamesArray}
+                        setShowSubmitMultiScore={setShowSubmitMultiScore}
+                        userArray={userArray}
+                    />
+                }
+            />
+            <Page>
+                <div className="section action-buttons">
+                    <div className="columns is-mobile">
+                        <div className="column is-one-fifth">
+                            <div>
+                                <div className="is-flex-direction-column">
+                                    <button
+                                        className="button is-warning is-medium action-button"
+                                        type="button"
+                                        onClick={() => setShowSubmitScore(true)}
+                                    >
+                                        Submit Score
+                                    </button>
+                                    <button
+                                        className="button is-danger is-medium action-button"
+                                        type="button"
+                                        onClick={() =>
+                                            setShowSubmitMultiScore(true)
+                                        }
+                                    >
+                                        Submit 3 Scores
+                                    </button>
+                                    <button
+                                        className="button is-info is-medium action-button"
+                                        type="button"
+                                        onClick={() => setShowRegister(true)}
+                                    >
+                                        Register User
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="column is-mobile">
+                            <div>
+                                <LatestGames gamesArray={gamesArray} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-            <div className="column is-mobile">
-              <div>
-                <LatestGames gamesArray={gamesArray} />
-              </div>
-            </div>
-          </div>
-        </div>
-        <Scoreboard userArray={userArray} />
-      </Page>
-    </>
-  );
+                <Scoreboard userArray={userArray} />
+            </Page>
+        </>
+    );
 }
 
 export default Home;
