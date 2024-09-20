c24d7ec8428d9c069d02ece18458b4b69418056d
Thomas McLaughlan <tmclaughlan@scottlogic.com>
2024-09-20 14:16:16 +0100

    Remove a console log

--- db/retrieveUsers/index.js
+++ db/retrieveUsers/index.js
@@ -47,7 +47,6 @@ async function closeConnection() {
 
 exports.handler = async (event, context) => {
     const isTest = event.queryStringParameters?.test === "true";
-    console.log(event, context);
 
     /* By default, the callback waits until the runtime event loop is empty before freezing the process and returning the results to the caller. Setting this property to false requests that AWS Lambda freeze the process soon after the callback is invoked, even if there are events in the event loop. AWS Lambda will freeze the process, any state data, and the events in the event loop. Any remaining events in the event loop are processed when the Lambda function is next invoked, if AWS Lambda chooses to use the frozen process. */
     context.callbackWaitsForEmptyEventLoop = false;
